e6507b5a316698f451fdedb439536efcf42c2184
bharath86repo <68587036+bharath86repo@users.noreply.github.com>
2023-09-26 15:35:54 +0530

    Add files via upload

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,10 +17,11 @@ public class SampleTest {
     
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() throws MalformedURLException{
     	ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        
         driver.manage().window().maximize();
         driver.get("https://google.com/");
     }
